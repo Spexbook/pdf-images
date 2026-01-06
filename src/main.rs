7025bafd59857576e142dd1da611053c16a88e82
frectonz <fraol0912@gmail.com>
2026-01-06 20:49:54 +0300

    feat: setup web api

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use axum::{
+    Router,
+    extract::{DefaultBodyLimit, Multipart},
+    routing::post,
+};
 use pdfium_render::prelude::*;
-use std::path::Path;
+use tower_http::limit::RequestBodyLimitLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-fn export_pdf_to_jpegs(path: impl AsRef<Path>) -> Result<(), PdfiumError> {
+fn export_pdf_to_jpegs(bytes: &[u8]) -> Result<(), PdfiumError> {
     let pdfium = Pdfium::default();
-
-    let document = pdfium.load_pdf_from_file(&path, None)?;
+    let document = pdfium.load_pdf_from_byte_slice(bytes, None)?;
 
     for (index, page) in document.pages().iter().enumerate() {
         page.render_with_config(&PdfRenderConfig::default())?
@@ -17,9 +22,37 @@ fn export_pdf_to_jpegs(path: impl AsRef<Path>) -> Result<(), PdfiumError> {
     Ok(())
 }
 
-fn main() {
-    export_pdf_to_jpegs(
-        "/Users/frectonz/Downloads/SURVEY (Certified) - Town North - 9.22.2025 (2).pdf",
-    )
-    .unwrap()
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/", post(handle_pdf_upload))
+        .layer(DefaultBodyLimit::disable())
+        .layer(RequestBodyLimitLayer::new(
+            250 * 1024 * 1024, /* 250mb */
+        ))
+        .layer(tower_http::trace::TraceLayer::new_for_http());
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn handle_pdf_upload(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let data = field.bytes().await.unwrap();
+    }
 }
