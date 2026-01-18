bf924e5d2864450ff657ce3241711cfc7978272b
frectonz <fraol0912@gmail.com>
2026-01-18 14:57:43 +0300

    chore: 0.1.1

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
         pkgs:
         let
           pname = "pdf-images";
-          version = "0.1.0";
+          version = "0.1.1";
         in
         rec {
           default = pkgs.rustPlatform.buildRustPackage {
