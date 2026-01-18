6fb440f0403c6e4785df463bafb09c6cfd1a6fd9
frectonz <fraol0912@gmail.com>
2026-01-18 09:29:53 +0300

    feat: add a `deploy` command to publish docker image

--- flake.nix
+++ flake.nix
@@ -48,52 +48,64 @@
         };
       });
 
-      packages = forAllSystems (pkgs: rec {
-        default = pkgs.rustPlatform.buildRustPackage {
+      packages = forAllSystems (
+        pkgs:
+        let
           pname = "pdf-images";
           version = "0.1.0";
-          src = pkgs.lib.cleanSourceWith {
-            src = self;
-            filter =
-              filePath: type:
-              let
-                baseName = baseNameOf filePath;
-              in
-              !builtins.elem baseName [
-                "flake.nix"
-                "flake.lock"
-                "README.md"
-                ".helix"
-                ".envrc"
-                ".gitignore"
-              ];
+        in
+        rec {
+          default = pkgs.rustPlatform.buildRustPackage {
+            inherit pname version;
+
+            src = pkgs.lib.cleanSourceWith {
+              src = self;
+              filter =
+                filePath: type:
+                let
+                  baseName = baseNameOf filePath;
+                in
+                !builtins.elem baseName [
+                  "flake.nix"
+                  "flake.lock"
+                  "README.md"
+                  ".helix"
+                  ".envrc"
+                  ".gitignore"
+                ];
+            };
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            postInstall = ''
+              wrapProgram $out/bin/pdf-images \
+                --set PDFIUM_DYNAMIC_LIB_PATH "${pkgs.pdfium-binaries}/lib"
+            '';
           };
 
-          cargoLock.lockFile = ./Cargo.lock;
-
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-
-          postInstall = ''
-            wrapProgram $out/bin/pdf-images \
-              --set PDFIUM_DYNAMIC_LIB_PATH "${pkgs.pdfium-binaries}/lib"
-          '';
-        };
-
-        docker = pkgs.dockerTools.buildLayeredImage {
-          name = "pdf-images";
-          tag = "latest";
-          contents = [
-            default
-            pkgs.cacert
-          ];
-          config = {
-            Cmd = [ "${default}/bin/pdf-images" ];
-            Env = [
-              "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = pname;
+            tag = "latest";
+            contents = [
+              default
+              pkgs.cacert
             ];
+            config = {
+              Cmd = [ "${default}/bin/pdf-images" ];
+              Env = [
+                "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              ];
+            };
           };
-        };
-      });
+
+          deploy = pkgs.writeShellScriptBin "deploy" ''
+            ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker-archive:${docker} docker://docker.io/frectonz/${pname}:${version} --dest-creds="frectonz:$ACCESS_TOKEN"
+            ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker://docker.io/frectonz/${pname}:${version} docker://docker.io/frectonz/${pname}:latest --dest-creds="frectonz:$ACCESS_TOKEN"
+          '';
+        }
+      );
 
       formatter = forAllSystems (
         pkgs:
